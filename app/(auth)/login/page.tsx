9e1b482486d74fb173cc604d5e9107d81ba9714d
mdrouhith <154613973+mdrouhith@users.noreply.github.com>
2026-02-15 22:40:31 +0600

    Create login page for user authentication functionality

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,21 @@
+// Login Page Component
+import React from 'react';
+
+const LoginPage = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <form>
+                <label htmlFor="username">Username:</label>
+                <input type="text" id="username" name="username" required />
+                <br />
+                <label htmlFor="password">Password:</label>
+                <input type="password" id="password" name="password" required />
+                <br />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default LoginPage;
